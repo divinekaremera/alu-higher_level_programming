bd73fdebde2a146c36d3397e9204a4d1bb9dae83
root <m.karemera@alustudent.com>
2023-11-30 12:44:38 -0800

    Initial commit

--- SQL_more_queries/103-rating_genres.sql
+++ SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,13 @@
--- Prints and joins records from two tables with a matching field
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-    FROM tv_shows
-    RIGHT JOIN tv_show_ratings
-        ON tv_show_ratings.show_id = tv_shows.id
-    GROUP BY tv_shows.title
-    ORDER BY rating DESC;
+-- lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+-- Each record should display: tv_genres.name - rating sum
+-- Results must be sorted in ascending order by their rating
+-- The database name will be passed as an argument of the mysql command
+
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.id
+ORDER BY rating DESC;
