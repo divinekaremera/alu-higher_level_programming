345370ee2b12db60908b69852d58e46d8a9ee5e7
root <m.karemera@alustudent.com>
2023-11-30 13:36:19 -0800

    Initial commit

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+"""A script that displays the value of the x-request ID."""
 
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+if __name__ == '__main__':
+    from urllib.request import urlopen
+    import sys
+
+    with urlopen(sys.argv[1]) as re:
+        header_var = re.headers.get('X-Request-Id')
+        print(header_var)
