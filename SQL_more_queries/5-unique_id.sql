eff11e4e1df2f349d9be0cf13364cd588940323e
root <m.karemera@alustudent.com>
2023-11-30 12:46:45 -0800

    Initial commit

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
--- Write a script that creates the table id_not_null on your MySQL server.
-CREATE TABLE IF NOT EXISTS `id_not_null` (id INT DEFAULT 1, name VARCHAR(256));
+-- creates the table unique_id on your MySQL server.
+CREATE TABLE IF NOT EXISTS unique_id (id INT UNIQUE DEFAULT 1, name VARCHAR(256));
